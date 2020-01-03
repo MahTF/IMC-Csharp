4f936b507d30466ba59aeae105d81c802b3fd355
mah.tf@hotmail.com <mah.tf@hotmail.com>
2020-01-03 19:52:26 -0300

    Criação e finalização do projeto

---
+++ IMC/IMC/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblClassificacao = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(19, 22);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(42, 16);
+            this.lblPeso.TabIndex = 0;
+            this.lblPeso.Text = "Peso:";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(19, 70);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(46, 16);
+            this.lblAltura.TabIndex = 4;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(175, 18);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 3;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(175, 66);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(175, 127);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 28);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular!";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoEllipsis = true;
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(19, 184);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(75, 18);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "Resposta";
+            this.lblResultado.Visible = false;
+            // 
+            // lblClassificacao
+            // 
+            this.lblClassificacao.AutoSize = true;
+            this.lblClassificacao.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold);
+            this.lblClassificacao.Location = new System.Drawing.Point(18, 272);
+            this.lblClassificacao.Name = "lblClassificacao";
+            this.lblClassificacao.Size = new System.Drawing.Size(56, 18);
+            this.lblClassificacao.TabIndex = 10;
+            this.lblClassificacao.Text = "Classe";
+            this.lblClassificacao.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 7F);
+            this.label1.Location = new System.Drawing.Point(67, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(ex: 73)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 7F);
+            this.label2.Location = new System.Drawing.Point(67, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "(ex: 154)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 6.5F);
+            this.label3.Location = new System.Drawing.Point(19, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Coloque somente os KG, sem vírgula";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 6.5F);
+            this.label4.Location = new System.Drawing.Point(19, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Coloque sua altura sem vírgula";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 299);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblClassificacao);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cálculo de indíce de massa corporal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblClassificacao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ IMC/IMC/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        double altura, peso, calculo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VerificarCampos(); 
+                
+                calculo = (peso / (altura * altura))*10000;
+                lblResultado.Visible = true; //Deixa o resultado visível.
+                
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("O campo PESO e ALTURA são obrigatórios e não devem permanecer vazios!", "Campo vazio!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtAltura.Text = "0";
+                txtPeso.Text = "0";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                lblResultado.Text = "Seu Indice de Massa Corporal é:\n" + calculo.ToString("N2");
+                CalcularClassificacao(calculo);
+            }
+        }
+
+        /// <summary>
+        /// Este método verifica se os campos estão irregulares e pede para efetuar a correção. 
+        /// </summary>
+        private void VerificarCampos()
+        {
+            if(String.IsNullOrEmpty(txtAltura.Text) || String.IsNullOrEmpty(txtPeso.Text))
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtPeso.Text, "[^0-9]"))
+            {
+                throw new Exception("O campo Peso deve ser preenchido somente com números");
+            }
+            if(System.Text.RegularExpressions.Regex.IsMatch(txtAltura.Text, "[^0-9]"))
+            {
+                throw new Exception("O campo Altura deve ser preenchido somente com números");
+            }
+
+            altura = double.Parse(txtAltura.Text.ToString());
+            peso = double.Parse(txtPeso.Text.ToString());
+
+            //De acordo com pesquisa realizada, a menor pessoa do mundo mede 62.8 cm e a mais alta 2,51
+            if (altura < 62 || altura > 260)
+            {
+                throw new Exception("Acreditamos que sua altura esteja errada, favor verificar!");
+            }
+            
+        }
+
+        /// <summary>
+        /// Este método realiza a classificação de risco.
+        /// </summary>
+        /// <param name="resultadoConta">Este valor é o resultado do cálculo do IMC. (Peso / altura²)</param>
+        private void CalcularClassificacao(double resultadoConta)
+        {
+            if (resultadoConta != 0)
+            {
+                lblClassificacao.Visible = true;
+            }
+
+            //Valores retirados de https://pt.wikipedia.org/wiki/%C3%8Dndice_de_massa_corporal 
+            if (resultadoConta < 17)
+            {
+                lblClassificacao.Text = "Muito abaixo do peso";
+                lblClassificacao.ForeColor = Color.Red;
+                return;
+            }
+            if(resultadoConta >= 17 && resultadoConta < 18.5)
+            {
+                lblClassificacao.Text = "Abaixo do peso";
+                lblClassificacao.ForeColor = Color.Orange;
+                return;
+            }
+
+            if (resultadoConta >= 18.5 && resultadoConta < 25)
+            {
+                lblClassificacao.Text = "Peso normal";
+                lblClassificacao.ForeColor = Color.Green;
+                return;
+            }
+            if (resultadoConta >= 25 && resultadoConta < 30)
+            {
+                lblClassificacao.Text = "Acima do peso";
+                lblClassificacao.ForeColor = Color.Blue;
+                return;
+            }
+            if (resultadoConta >= 30 && resultadoConta < 35)
+            {
+                lblClassificacao.Text = "Obesidade";
+                lblClassificacao.ForeColor = Color.Purple;
+                return;
+            }
+            if (resultadoConta >= 35)
+            {
+                lblClassificacao.Text = "Obesidade II";
+                lblClassificacao.ForeColor = Color.MidnightBlue;
+                if(resultadoConta >= 40)
+                {
+                    lblClassificacao.Text = "Obesidade III";
+                    MessageBox.Show("Aconselho a procura de um profissional para aconselhamento!", "Estado grave!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                return;
+            }
+        }
+    }
+}
